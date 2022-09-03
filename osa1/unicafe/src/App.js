2dd8a2a3cf61405158e69d766dae864a14199d3d
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-09-03 22:33:24 +0300

    tehtävä 1.7
    1.7: unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -7,13 +7,32 @@ const Button = ({handleClick, text}) => {
 }
 
 const Statistics = (props) => {
+  
+  const sumAll = () => props.good + props.neutral + props.bad
+
+  const average = () => {
+    if (sumAll() === 0) return 0
+    return (props.good * 1 + props.bad * -1) / sumAll()
+  }
+
+  const positivePct = () => {
+    if (props.good === 0) return 0 
+    return props.good / sumAll() * 100
+  }
+
   return (
   <div>
     <h1>Statistics</h1>
     <br/>
+
     <p>good {props.good}</p>
     <p>neutral {props.neutral}</p>
     <p>bad {props.bad}</p>
+
+    <p>all {sumAll()}</p>
+    <p>average {average()}</p>
+    <p>positive {positivePct()}%</p>
+
   </div>
   )
 }
@@ -41,7 +60,7 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      
+
       <Button handleClick={increaseGood} text="good" />
       <Button handleClick={increaseNeutral} text="neutral" />
       <Button handleClick={increaseBad} text="bad" />
