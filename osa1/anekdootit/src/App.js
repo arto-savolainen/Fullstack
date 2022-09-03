4068d23e91d0ffef009043420bfb604c146072e7
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-09-04 00:27:46 +0300

    tehtävä 1.12
    1.12*: anekdootit step1

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const Button = ({handleCLick, text}) => {
+  return (
+    <button onClick={handleCLick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max)
+  }
+
+  const nextAnecdote = () => {
+    if (anecdotes.length < 2) return
+
+    let i = selected;
+    while (i === selected) {
+      i = getRandomInt(anecdotes.length)
+    }
+    
+    setSelected(i)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <Button handleCLick={nextAnecdote} text="next anecdote" />
+    </div>
+  )
+}
+
+export default App
