e523999df88ada5ea5395bab414df46b7164bf14
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-09-08 18:27:11 +0300

    tehtävät 2.11
    2.11: puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DisplayPerson = ({ person }) => {
   return (
@@ -36,18 +37,18 @@ const NewPersonForm = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '0500'
-    },
-    { 
-      name: 'Arto Savolainen',
-      number: 'xxxxxxxxxxx' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
