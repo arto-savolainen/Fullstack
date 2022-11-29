1085b63d59db9b4a66ea7e775795d8ead789e20a
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-11-29 16:25:44 +0200

    tehtävä 6.1 ja 6.2
    6.1: Unicafe revisited, step1
    6.1: Unicafe revisited, step2
    
    good ok bad reset painikkeet toimivat ja testit niille tehty

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,57 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('state is reset to zeroes', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+
+    const action2 = {
+      type: 'ZERO'
+    }
+
+    deepFreeze(newState)
+    const newState2 = counterReducer(newState, action2)
+    expect(newState2).toEqual(initialState)
+  })
 })
